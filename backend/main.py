c1574c5903d43ce16dedaab979dcee61f7634f48
iambiwash <iambiwash@gmail.com>
2024-09-16 17:29:24 -0500

    functions and env setup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
-from fastapi import FastAPI
+#uvicorn main:app
+#uvicorn main:app --reload
 
+#main imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+
+#Custom Function Imports
+
+#Initiate App
 app = FastAPI()
+
+#CORS - Origin
+origins = [
+    "https://localhost:5173",
+    "https://localhost:5174",
+    "https://localhost:4173",
+    "https://localhost:4174",
+    "https://localhost:3000"
+]
 
 
 @app.get("/")
